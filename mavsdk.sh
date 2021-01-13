c824e2d8667a26d742e8b5a02642f9b931354005
HETONGAPP <44877369+HETONGAPP@users.noreply.github.com>
2021-01-12 20:34:57 -0500

    Add files via upload

---
+++ mavsdk.sh
@@ -0,0 +1,4 @@
+cd /home/$USER/MAVSDK
+unzip debian.zip && unzip docker.zip  && unzip examples.zip && unzip proto.zip && unzip src.zip && unzip templates.zip && unzip third_party.zip && unzip tools.zip
+rm debian.zip && rm docker.zip  && rm examples.zip && rm proto.zip && rm src.zip && rm templates.zip && rm third_party.zip && rm tools.zip
+
